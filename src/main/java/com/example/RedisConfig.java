f2256510ecb0d6205ad3acdf59cb4d105f7ce6b6
heowc <heowc1992@gmail.com>
2020-09-10 15:51:12 +0900

    Apply `LettuceClientConfiguration`

--- src/main/java/com/example/RedisConfig.java
+++ src/main/java/com/example/RedisConfig.java
@@ -1,25 +1,33 @@
 package com.example;
 
+import io.lettuce.core.resource.DefaultClientResources;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {
 
+    public static final int POOL_SIZE = 100;
+
     @Bean
     RedisConnectionFactory redisConnectionFactory() {
         RedisClusterConfiguration clusterConfiguration = new RedisClusterConfiguration();
-        clusterConfiguration.clusterNode("127.0.0.1", 7000);
+        clusterConfiguration.clusterNode("0.0.0.0", 7000);
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .clientResources(DefaultClientResources.builder().ioThreadPoolSize(POOL_SIZE).computationThreadPoolSize(POOL_SIZE).build())
+                .build();
+//        return new LettuceConnectionFactory(clusterConfiguration, clientConfig);
         return new LettuceConnectionFactory(clusterConfiguration);
     }
 
     @Bean
-    RedisTemplate<String, Integer> redisTemplate() {
-        final RedisTemplate<String, Integer> template = new RedisTemplate<>();
+    RedisTemplate<String, String> redisTemplate() {
+        final RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory());
         return template;
     }
--- src/test/java/com/example/ApplicationTests.java
+++ src/test/java/com/example/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StopWatch;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -21,23 +23,23 @@ public class ApplicationTests {
     private static final Logger logger = LoggerFactory.getLogger(ApplicationTests.class);
 
     @Autowired
-    private RedisTemplate<String, Integer> redisTemplate;
+    private RedisTemplate<String, String> redisTemplate;
 
     @Test
     public void contextLoads() throws Exception {
-        final ExecutorService executorService = Executors.newFixedThreadPool(100);
+        final ExecutorService executorService = Executors.newFixedThreadPool(200);
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000000; i++) {
             final int number = i;
             executorService.submit(() -> {
                 final String key = String.valueOf(number);
-                redisTemplate.opsForValue().set(key, number);
-                logger.info("GET {}",redisTemplate.opsForValue().get(key));
+                redisTemplate.opsForValue().set(key, RandomStringUtils.random(100));
+//                logger.info("GET {}",redisTemplate.opsForValue().get(key));
                 redisTemplate.delete(key);
             });
         }
 
-        Thread.sleep(1000 * 10L);
+        Thread.sleep(1000 * 20L);
     }
 
 }
